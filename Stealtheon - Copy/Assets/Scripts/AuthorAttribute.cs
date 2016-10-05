f0532bcab0636935a84ecb3a0e258fc3ab25b2ae
ja179053 <ja179053@falmouth.ac.uk>
2016-10-05 13:26:28 +0100

    Combined Zacs new Title screen
    Looking good- needs drum roll music

---
+++ Stealtheon - Copy/Assets/Scripts/AuthorAttribute.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Reflection;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+public class AuthorAttribute : Attribute
+{
+	string sayMyName;
+
+	public AuthorAttribute (string sayMyName)
+	{
+		this.sayMyName = sayMyName;
+	}
+
+	public string SayMyName {
+		get {
+			return sayMyName;
+		}
+	}
+
+	public override string ToString ()
+	{
+		string value = "Author : " + SayMyName;
+		return  value;
+	}
+}
