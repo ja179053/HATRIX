89be4ebbf38b7d0bb3f7f1694bf63b7b141abc53
ja179053 <ja179053@falmouth.ac.uk>
2016-10-24 15:03:15 +0100

    Extra from last commit

--- Stealtheon - Copy/Assets/Scripts/Title.cs
+++ Stealtheon - Copy/Assets/Scripts/Title.cs
@@ -1,17 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
-
+//Controls the level loaded from the title screen
 [AuthorAttribute ("JJ", TeamRole.Programmer)]
-public class Title : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+public class Title : MonoBehaviour
+{	
+	void Update ()
+	{
 		if (Input.GetKey (KeyCode.Escape)) {
 			Application.Quit ();
 		} else if (Input.anyKeyDown) {
