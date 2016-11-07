3e2d955928fefe97d323293a8d2699fb7fcf0c4d
ja179053 <ja179053@falmouth.ac.uk>
2016-11-07 12:31:21 +0000

    Added next level key reset

--- Stealtheon - Copy/Assets/Scripts/Puzzles/NextLevelTrigger.cs
+++ Stealtheon - Copy/Assets/Scripts/Puzzles/NextLevelTrigger.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class NextLevelTrigger : MonoBehaviour {
 	void OnTriggerEnter(Collider c){
 		if (ActorBehaviour.gotKey) {
+			ActorBehaviour.gotKey = false;
 			if (c.gameObject.gameObject.tag == "Player") {
 				StartCoroutine (Title.NewLevel ());
 			}
