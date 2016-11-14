e88edc23c19f8466f98288cb03d3f36f32dc291f
ja179053 <ja179053@falmouth.ac.uk>
2016-11-14 14:07:24 +0000

    Testing scene, more scripts and updated

---
+++ Stealtheon - Copy/Assets/Scripts/Tools/LookAtScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtScript : MonoBehaviour {
+	public Transform target;
+	public Vector3 extra;
+	public Quaternion targetRotation;
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (target == null) {
+			transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.fixedDeltaTime);
+		} else {
+			transform.LookAt (target.position + extra);
+		}
+	}
+}
