4f24b5faea8dc7a4ce17f5c3ec7476b7487b4a6c
zgo11084339 <zgo11084339@gmail.com>
2016-11-29 15:35:40 +0000

    Zac's Home Unity
    Art, and models, and whitebox and code and stuff, and things.

---
+++ Hunger Dog Unity/Assets/transition1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class transition1 : MonoBehaviour {
+
+	public GameObject player;
+	
+	
+	void Start () 
+	{
+		
+	}
+	
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter (Collider Other) 
+	{
+		
+		if (Other.gameObject.tag == "Player") 
+		{   
+			player.SendMessage ("InRoom3");  
+			Debug.Log ("work");
+		}
+	}
+}
