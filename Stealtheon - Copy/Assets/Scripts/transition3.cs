6e50ac17dda951bc283b9ef48a1a2419bde7e3f1
ja179053 <ja179053@falmouth.ac.uk>
2016-12-02 15:35:57 +0000

    Imported Zacs Art
    New Light prefab. need to make in-game curtains

---
+++ Stealtheon - Copy/Assets/Scripts/transition3.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class transition3 : MonoBehaviour {
+
+	public GameObject player;
+	
+	
+	void Start () 
+	{
+		
+	}
+	
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter (Collider Other) 
+	{
+		
+		if (Other.gameObject.tag == "Player") 
+		{   
+			player.SendMessage ("InRoom1");  
+			Debug.Log ("work");
+		}
+	}
+}
