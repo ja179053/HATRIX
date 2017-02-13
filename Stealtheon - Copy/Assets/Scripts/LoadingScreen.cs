7f2a83cd765e923321f7810957ddf29e6835d109
ja179053 <ja179053@falmouth.ac.uk>
2017-02-13 20:36:09 +0000

    New example cover scene

--- Stealtheon - Copy/Assets/Scripts/LoadingScreen.cs
+++ Stealtheon - Copy/Assets/Scripts/LoadingScreen.cs
@@ -2,30 +2,31 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class LoadingScreen : MonoBehaviour {
+[AuthorAttribute("JJ", TeamRole.Programmer)]
+public class LoadingScreen : MonoBehaviour
+{
 	static Image myImage;
 	// Use this for initialization
-	void Start () {
-	//	DontDestroyOnLoad (this.gameObject);
+	void Start ()
+	{
+		//	DontDestroyOnLoad (this.gameObject);
 		myImage = GetComponent<Image> ();
 		myImage.color = new Vector4 (myImage.color.r, myImage.color.g, myImage.color.b, 0);
 	}
 	
 	// Update is called once per frame
-	public static IEnumerator AsynchronousLoad (int scene, float time) {
+	public static IEnumerator AsynchronousLoad (int scene, float time)
+	{
 		AsyncOperation ao = SceneManager.LoadSceneAsync (scene);
 		myImage.color = new Vector4 (myImage.color.r, myImage.color.g, myImage.color.b, 1);
 		ao.allowSceneActivation = false;
 		yield return new WaitForSeconds (time);
 		while (!ao.isDone) {
 			if (ao.progress == 0.9f) {
-			//	Debug.Log ("loaded");
-			//	if (Input.anyKey) {
-					ao.allowSceneActivation = true;
-			//	}
+				//	Debug.Log ("loaded");
+				ao.allowSceneActivation = true;
 			}
-		//	myImage.color = new Vector4 (myImage.color.r, myImage.color.g, myImage.color.b, 0);
-			yield return new WaitForEndOfFrame();
+			yield return new WaitForEndOfFrame ();
 		}
 	}
 }
