8e12d3da2b56310c022e532dfbfa67aa1fda6019
ja179053 <ja179053@falmouth.ac.uk>
2017-02-21 13:26:04 +0000

    Cup trick scene

---
+++ Stealtheon - Copy/Assets/Scripts/Minigames/Cups/Cup.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cup : MonoBehaviour {
+	Vector3 newPos;
+	// Use this for initialization
+	void Start () {
+		newPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (newPos != null) {
+			transform.position = Vector3.MoveTowards (transform.position, newPos, Time.deltaTime * CupsGame.difficulty * 2);	
+		}
+	}
+	public void Swap(Vector3 destination){
+		Debug.Log (destination + this.name);
+		newPos = destination;
+	//	MoveTo (destination);
+	//	StartCoroutine (MoveTo (destination));
+	//	transform.position = newPos;
+	}
+	public IEnumerator Reveal(){
+		transform.position += (Vector3.up * 5);
+		yield return new WaitForSeconds (4);
+		transform.position += (Vector3.up * -5);
+		newPos = transform.position;
+	}
+
+	void MoveTo(Vector3 destination){
+		if (transform.position == destination) {
+			transform.position = Vector3.MoveTowards (transform.position, destination, Time.deltaTime * CupsGame.difficulty * 2);	
+		} else {
+			MoveTo (destination);
+		}
+	}
+	void OnMouseDown(){
+		if (!CupsGame.running) {
+			//Show ();
+			CupsGame.DecideWin(this);
+		}
+	}
+}
---
+++ Stealtheon - Copy/Assets/Scripts/Minigames/Cups/CupsGame.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CupsGame : Singleton<CupsGame> {
+	public static Cup[] cups;
+	public static int ballNum, difficulty = 3;
+	static int Difficulty{
+		get{ return difficulty; } set {
+			difficulty = value;	
+			Restart (difficulty);
+		}
+	}
+	public static GameObject ball;
+	public static bool running = false;
+	// Use this for initialization
+	void Start () {
+		if (ball == null) {
+			ball = GameObject.Find ("Ball");
+		}
+		cups = FindObjectsOfType<Cup> ();
+		Restart (difficulty);
+	}
+	public static void Restart(int times){
+		running = true;
+		Instance.StartCoroutine(MoveBall (times));
+	}
+	public static IEnumerator Swap(int times){
+		int i = Random.Range (0, 3);
+		int i2 = i;
+		while (i2 == i) {
+			i2 = Random.Range (0, 3);
+		}
+		Vector3 tempPos = cups [i].transform.position;
+		cups[i].Swap(cups[i2].transform.position);
+		cups [i2].Swap (tempPos);
+		yield return new WaitForSeconds (5 / difficulty);
+		if (times > 1) {
+			Instance.StartCoroutine(Swap(times - 1));
+			yield break;
+		}
+		running = false;
+	}
+	static IEnumerator MoveBall(int times){
+		ballNum = Random.Range (0, 3);
+		ball.transform.parent = cups [ballNum].transform;
+		ball.transform.localPosition = Vector3.zero;
+		ball.transform.parent = Instance.transform;
+		Instance.StartCoroutine(cups [ballNum].Reveal());
+		yield return new WaitForSeconds (5);
+		ball.transform.parent = cups [ballNum].transform;
+		Instance.StartCoroutine (Swap (times));
+	}
+	public static void DecideWin(Cup cup){
+		if (cups [ballNum] == cup) {
+			Debug.Log ("You win");
+			Difficulty++;
+		} else {
+			Debug.Log ("You lose");
+			Difficulty--;
+		}
+	}
+}
