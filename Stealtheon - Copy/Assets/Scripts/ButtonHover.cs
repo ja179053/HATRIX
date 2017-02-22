b46946c87fbcaba4a72f912d45ea53bec1d8c45a
ja179053 <JA179053@falmouth.ac.uk>
2017-02-22 16:13:45 +0000

    Title juice

---
+++ Stealtheon - Copy/Assets/Scripts/ButtonHover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Events;
+public class ButtonHover : Singleton<ButtonHover> {
+	Vector3 startPos, endPos;
+	Button activeButton;
+	public float speed = 1;
+	public void Activate(Button b){
+		activeButton = b;
+		b.transform.localScale = new Vector3 (2, 2, 1);
+		startPos = b.transform.position;
+		endPos = startPos + (Vector3.right * speed);
+	//	if (pos ) {
+	//		pos = b.transform.localPosition;
+	//	}
+	}
+	void Update(){
+		Wiggle ();	
+	}
+	void Wiggle(){
+		if(activeButton != null){
+	//		activeButton.transform.position = Vector3.Lerp (startPos, endPos, Mathf.SmoothStep (0, speed, Mathf.SmoothStep (0, speed, Time.time)));
+			activeButton.transform.position = startPos + (Vector3.right * Mathf.Sin(Time.timeSinceLevelLoad / speed) * speed);
+		}
+	}
+	public void Deactivate(Button b){
+		b.transform.localScale = new Vector3 (1, 1, 1);
+		b = null;
+	}
+}
